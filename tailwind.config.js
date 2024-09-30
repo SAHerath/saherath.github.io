b0031cb1ebd8c2d2902567c63e880165b40be7bf
SAHerath <h.supun.anuradha@gmail.com>
2024-09-30 22:06:49 +0530

    add custom styles (screen sizes and border radius)

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,24 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
+    borderRadius: {
+      'none': '0',
+      'sm': '0.125rem',
+      DEFAULT: '0.25rem',
+      DEFAULT: '4px',
+      'md': '0.375rem',
+      'lg': '0.5rem',
+      'full': '9999px',
+      'large': '12px',
+      'vl': '3rem',
+    },
     extend: {
       fontFamily: {
         nunito: ['nunito', 'sans-serif'],
