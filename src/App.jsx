65b0003fb8672de52d87f1f24c16bf0928a05294
SAHerath <h.supun.anuradha@gmail.com>
2024-10-03 13:14:07 +0530

    add dummy Home and About pages through react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import { useState } from "react";
-import Header from "./components/Header"; 
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import "./App.css";   
 
 function App() {
   const [count, setCount] = useState(0);
@@ -7,6 +11,12 @@ function App() {
   return (
     <>
       <Header />
+
+      <Routes>
+        <Route path="/sah-portfolio" element={<Home />} />
+        <Route path="/sah-portfolio/about" element={<About />} />
+      </Routes>
+
 
     </>
   );
---
+++ src/pages/About.jsx
@@ -0,0 +1,7 @@
+const About = () => {
+  return ( 
+    <h1 className="text-xl text-white p-16">About Page</h1>
+   );
+}
+ 
+export default About;
