8b8846d8b401617da774ef81c103fc1f3bb38a15
SAHerath <h.supun.anuradha@gmail.com>
2024-10-18 10:25:32 +0530

    add dummy projects and contact pages

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <>
+      <h1 className="text-xl text-white p-16">Contact Me</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam porttitor iaculis eros ut laoreet. Phasellus blandit lorem sit amet nisl luctus ullamcorper. Praesent suscipit semper sodales. Aenean ac magna massa. Proin maximus scelerisque nibh, quis auctor risus dapibus vel. Aliquam fringilla porta enim, eget sagittis metus scelerisque sit amet. Suspendisse tincidunt vestibulum orci, eu porta turpis imperdiet bibendum. Fusce suscipit ac eros aliquam tempor. Mauris dictum, ipsum non mollis tincidunt, nibh nunc malesuada neque, sed tempor velit neque ut nibh. 
+      </p>
+      <p>
+        Mauris et massa et turpis aliquet scelerisque. Duis at fermentum tortor, ac vestibulum lacus. Etiam in nisl varius, feugiat sapien id, feugiat nulla. Proin ullamcorper dolor in est fringilla rhoncus. Pellentesque scelerisque lacus nec lectus aliquam luctus. Quisque iaculis consectetur pharetra. Nulla facilisi. Praesent dolor erat, convallis sed leo quis, fringilla porta nisl. 
+      </p>
+      <p>
+        Vestibulum massa libero, vehicula pharetra arcu ac, lobortis laoreet eros. Integer aliquam velit non tellus tristique, vulputate rutrum neque mollis. Quisque ex risus, lacinia sed vehicula sed, fermentum non leo. Nunc tempus mi id purus commodo molestie. Pellentesque sit amet velit non felis condimentum fringilla. Nam dignissim ac lectus eget dignissim. Nulla facilisi. Vestibulum in augue tempus, dignissim urna sed, iaculis elit. Curabitur a odio aliquet, fermentum purus in, tincidunt sapien. Ut vel iaculis neque. Donec sagittis, tortor non condimentum hendrerit, quam libero fringilla nisi, in egestas lorem tellus eu ante. Phasellus volutpat aliquam eros. Quisque nulla est, consectetur ac egestas vitae, dapibus a libero. 
+      </p>
+      <p>
+        In eu est ut massa scelerisque laoreet id eu lorem. Pellentesque laoreet mollis arcu, eu gravida dolor. In diam erat, aliquet sit amet vestibulum id, ultricies eu felis. Nunc ultricies lacus felis, at rhoncus justo gravida ac. Sed dui nulla, convallis vel finibus ut, varius in dolor. Aliquam eu semper felis. In pulvinar tempor velit nec tincidunt. Proin lectus libero, volutpat quis consequat eu, imperdiet sed nunc. Nullam at lacus eget elit tincidunt tempus. Praesent nec justo at dui posuere ornare sit amet eu ligula. Cras ac justo sollicitudin, fermentum libero vel, dignissim libero. 
+      </p>
+    </>
+  )
+}
+
+export default Contact;
