babf2893ab794dd7310ea5a2445be5d124927981
SAHerath <h.supun.anuradha@gmail.com>
2024-10-18 11:46:28 +0530

    add mobile menu toggle function

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,4 @@
+import React, {useState} from "react";
 import { Link as LinkRouter } from "react-router-dom";
 import { Link as LinkScroll } from "react-scroll";
 import {PiListBold, PiXBold} from "react-icons/pi";
@@ -5,6 +6,11 @@ import Logo from "/logo/AH.svg";
 
 const Header = () => {
 
+    const [menuOpen, setMenuOpen] = useState(false);
+    
+    const toggleMenu = () => { 
+        setMenuOpen(!menuOpen);
+    }
     const handleResume = () => {
         console.log("hello handleResume!");
     }
@@ -37,31 +43,31 @@ const Header = () => {
                             <a onClick={handleResume} className="px-4 py-2 rounded-md cursor-pointer text-white bg-green-600 hover:text-black hover:shadow-glow-green">Resume</a>
                         </li>
                         <li className="sm:hidden">
-                            <a href="" className="cursor-pointer text-2xl group">
-                                {/* <i className="block group-[.open]:hidden">{icons.menuOpen}</i>
-                                <i className="hidden group-[.open]:block">{icons.menuClose}</i> */}
+                            <a onClick={toggleMenu} className={"cursor-pointer text-2xl group" + (menuOpen ? " open" : "")}>
                                 <i className="block group-[.open]:hidden"><PiListBold/></i>
                                 <i className="hidden group-[.open]:block"><PiXBold/></i>
                             </a>
                         </li>
                     </ul>
                 </div>
+                {menuOpen && 
                 <div className="pb-2 sm:hidden">
                     <ul className="flex flex-col items-stretch px-8 sm:px-16 text-lg">
                         <li>
-                            <LinkScroll to="home" spy={true} smooth={true} duration={500} className="block py-1 rounded-full hover:bg-gradient-to-t from-black text-white">Home</LinkScroll>
+                            <LinkScroll onClick={toggleMenu} to="home" spy={true} smooth={true} duration={500} className="block py-1 rounded-full hover:bg-gradient-to-t from-black text-white">Home</LinkScroll>
                         </li>
                         <li>
-                            <LinkScroll to="about" spy={true} smooth={true} duration={500} className="block py-1 rounded-full hover:bg-gradient-to-t from-black">About</LinkScroll>
+                            <LinkScroll onClick={toggleMenu} to="about" spy={true} smooth={true} duration={500} className="block py-1 rounded-full hover:bg-gradient-to-t from-black">About</LinkScroll>
                         </li>
                         <li>
-                            <LinkScroll to="projects" spy={true} smooth={true} duration={500} className="block py-1 rounded-full hover:bg-gradient-to-t from-black">Projects</LinkScroll>
+                            <LinkScroll onClick={toggleMenu} to="projects" spy={true} smooth={true} duration={500} className="block py-1 rounded-full hover:bg-gradient-to-t from-black">Projects</LinkScroll>
                         </li>
                         <li>
-                            <LinkScroll to="contact" spy={true} smooth={true} duration={500} className="block py-1 rounded-full hover:bg-gradient-to-t from-black">Contact</LinkScroll>
+                            <LinkScroll onClick={toggleMenu} to="contact" spy={true} smooth={true} duration={500} className="block py-1 rounded-full hover:bg-gradient-to-t from-black">Contact</LinkScroll>
                         </li>
                     </ul>
                 </div>
+                }
 
             </nav>
 
