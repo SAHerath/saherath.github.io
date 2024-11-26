0076b349466328442557d00e1d976460c17079f3
SAHerath <h.supun.anuradha@gmail.com>
2024-11-27 00:27:28 +0530

    update home page, add profile picture

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,20 @@
+import ProfileImg from "../assets/images/avatar3.png";
+
 const Home = () => {
   return ( 
-    <>
-      <h1 className="text-xl text-white p-16">Home Page</h1>
-    </>
+    <section>
+      <div className="pt-12 md:px-16 flex items-center justify-around flex-col md:flex-row">
+        <div className="font-nunito text-center md:text-left">
+          <h1 className="text-4xl sm:text-6xl font-semibold">Hello</h1>
+          <div className="py-4">
+            <h2 className="text-3xl sm:text-5xl">Im Anuradha Herath</h2>
+          </div>
+        </div>
+        <div className="w-80 bg-gradient-to-t from-black rounded-vl overflow-hidden backdrop-blur-md">
+            <img src={ProfileImg} alt="Profile Photo" />
+        </div>
+      </div>
+    </section>
    );
 }
  
