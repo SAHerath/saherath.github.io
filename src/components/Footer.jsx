8c8071b6d807e36e02efb4db23c2acd3b840ebec
SAHerath <h.supun.anuradha@gmail.com>
2025-01-01 12:41:45 +0530

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <>
+    <div className="flex flex-col items-center gap-4 pt-24 pb-4">
       <h4>© Copyright 2024 Developed by <b>SAH</b></h4>
-    </>
+    </div>
   )
 }
 
