98b2bb309c1e3c9dd7e14a136abfce6b8821a5d8
SAHerath <h.supun.anuradha@gmail.com>
2025-03-23 12:32:37 +0530

    optimized by size

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,16 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: "/",
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          "react-vendors": ["react", "react-dom", "react-router-dom"],
+          "ui-libs": ["react-icons", "react-scroll", "typewriter-effect"],
+          "analytics": ["react-ga4"],
+        },
+      },
+    },
+  },
 })
